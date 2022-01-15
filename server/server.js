b591c6e26fed3360325fd88aa00a6cde834f7ea8
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2022-01-15 09:04:18 -0500

    build: Initialized app

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+// MongoDB Connection
+
+
+// API 
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+
+// Connection
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+})
