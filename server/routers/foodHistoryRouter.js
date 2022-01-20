6a7c0d0aa574fde3449d0bf5a263aa1bed29ffac
Tenzin Tashi <tenztashi8@gmail.com>
2022-01-20 15:48:10 -0500

    build: server api for dailyintake

--- server/routers/foodHistoryRouter.js
+++ server/routers/foodHistoryRouter.js
@@ -5,59 +5,19 @@ import foodHistoryModel from "../models/foodHistoryModel.js";
 const foodHistoryRouter = express.Router();
 
 foodHistoryRouter.get(
-  "/get/user/",
+  "/get/",
   expressAsyncHandler(async (req, res, err) => {
     // Some Code in here
-    const users_email = await foodHistoryModel.find({
-      user_email: req.body.user_email,
-    });
-    if (users_email) {
-      return res.send(users_email.history);
-    }
-    return res.status(404).send(err.stack);
+    return res.send('food history get methods');
+    
   })
 );
 
 foodHistoryRouter.post(
-  "/post/foodhistory/",
+  "/post/",
   expressAsyncHandler(async (req, res, err) => {
     // Some Code in here
-    foodHistoryModel.exists(
-      { user_email: req.body.user_email },
-      function (err, result) {
-        if (err) {
-          res.send(err);
-        } else {
-          if (result) {
-            console.log("found");
-            // If found append it to the existing one
-            foodHistoryModel.findOneAndUpdate(
-              { user_email: req.body.user_email },
-              { $push: { history: req.body.history } },
-              { upsert: true, new: true, setDefaultsOnInsert: true },
-              function (err, result) {
-                if (err) res.send(400, { error: err });
-                else return res.send(200, { response: result });
-              }
-            );
-          } else {
-            console.log("not FOUND");
-            // If not found create new entry in database
-            const foodHistorySchemaModel = foodHistoryModel(req.body);
-            foodHistorySchemaModel
-              .save()
-              .then((result) => {
-                console.log("result =>", result);
-                res.send(200, { response: result });
-              })
-              .catch((err) => {
-                console.log("err =>", err);
-                res.send({ error: err });
-              });
-          }
-        }
-      }
-    );
+    return res.send('food history post methods');
   })
 );
 
