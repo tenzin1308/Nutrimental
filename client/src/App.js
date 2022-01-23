a3419141362cf8e6d6a893ee6ed07202946ba4aa
Justin <justinparkcs@gmail.com>
2022-01-23 17:02:57 -0500

    feat: modified UI for signin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from './pages/Home.js';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-
+import Home from "./pages/Home.js";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<SignIn />} />
-          <Route exact path="/signup" element={<SignUp />} />
-        </Routes>
-      </Router> 
+        <div className="flex flex-col w-screen h-screen">
+          <div>
+            <NavBar />
+          </div>
+          <div className="h-full">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<SignIn />} />
+              <Route exact path="/signup" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </>
   );
 }
