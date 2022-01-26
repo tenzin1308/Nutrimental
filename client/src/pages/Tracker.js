d631cb353463eafd237ca9dcbc39809cb77ac371
Justin <justinparkcs@gmail.com>
2022-01-26 17:00:44 -0500

    fix: integrated profile page with accountlayout and fix styling

--- client/src/pages/Tracker.js
+++ client/src/pages/Tracker.js
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AccountLayout from "../components/AccountLayout";
 
+const TABS = [
+  "Macro Tracker",
+  "Nutrient Tracker",
+  "Nutrient Advice",
+  "Food History",
+];
+
 const Tracker = () => {
-  const tabs = [
-    "Macro Tracker",
-    "Nutrient Tracker",
-    "Nutrient Advice",
-    "Food History",
-  ];
+  const [selectedTab, setSelectedTab] = React.useState(TABS[0]);
+
+  useEffect(() => {}, [selectedTab]);
 
   return (
-    <AccountLayout tabsLst={tabs}>
-      {/* INSERT CHILDREN */}
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
-      <h1>ITS WORKING TRACKER PAGE</h1>
+    <AccountLayout
+      tabsLst={TABS}
+      handleTabUpdate={setSelectedTab}
+      selectedTab={selectedTab}
+    >
+      <div className="p-4">
+        <h1 className=" text-5xl">ITS WORKING!! THE TRACKER PAGE</h1>
+        <br />
+        {selectedTab === "Macro Tracker" ? (
+          <div>
+            <h1>{selectedTab}</h1>
+          </div>
+        ) : selectedTab === "Nutrient Tracker" ? (
+          <div>
+            <h1>{selectedTab}</h1>
+          </div>
+        ) : selectedTab === "Nutrient Advice" ? (
+          <div>
+            <h1>{selectedTab}</h1>
+          </div>
+        ) : (
+          selectedTab === "Food History" && (
+            <div>
+              <h1>{selectedTab}</h1>
+            </div>
+          )
+        )}
+      </div>
     </AccountLayout>
   );
 };
