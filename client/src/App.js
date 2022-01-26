b67b5729bf759ff2c61ada685cfa3ec2240542c3
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2022-01-26 14:24:43 -0500

    build: profile page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Account } from "./components/Account";
 import NavBar from "./components/NavBar";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home.js";
+import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
@@ -36,6 +37,7 @@ function App() {
               <Route exact path="/login" element={<Account><SignIn authProps={authProps} setAuthProps={setAuthProps} /></Account>} />
               <Route exact path="/signup" element={<Account><SignUp authProps={authProps} setAuthProps={setAuthProps} /></Account>} />
               <Route exact path="/dashboard" element={<Dashboard authProps={authProps} setAuthProps={setAuthProps} />} />
+              <Route exact path="/profile" element={<Profile authProps={authProps} setAuthProps={setAuthProps} />} />
             </Routes>
           </div>
         </div>
