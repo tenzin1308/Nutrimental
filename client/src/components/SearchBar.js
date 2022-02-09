4c163c7df838b1bbb5625835245cc2e72e732c07
Justin <justinparkcs@gmail.com>
2022-02-08 19:56:02 -0500

    search bar logic done. need searched page

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,16 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
-const SearchBar = () => {
+const SearchBar = ({}) => {
   const [searchTxt, setSearchTxt] = useState("");
+  const [sendData, setSendData] = useState({});
+  const [vitaminData, setVitaminData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  // Logic made to check vitamin DB first.
+  // If not in vitamin DB, checks the gov DB
+  // Returns final data-sets in sendData
+  // check loading.. if true, data isnt done.
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(searchTxt);
+
+    getVitaminDbData();
   };
+
+  useEffect(() => {
+    if (vitaminData.length === 0) {
+      getGovApiData();
+    } else {
+      setSendData(vitaminData);
+    }
+  }, [vitaminData]);
 
   const handleChange = (event) => {
     setSearchTxt(event.target.value);
   };
+
+  const getGovApiData = async () => {
+    await axios
+      .get(
+        // pageSize range = [1, 200]
+        `https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchTxt}&pageSize=10&api_key=${process.env.REACT_APP_USDA_GOV_API_KEY}`
+      )
+      .then((res) => {
+        setSendData(res.data.foods);
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  };
+
+  const getVitaminDbData = async () => {
+    await axios
+      .get(`http://localhost:8000/api/vitamin/get?vitamin_name=${searchTxt}`)
+      .then((res) => {
+        setVitaminData(res.data);
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  };
+
+  useEffect(() => {
+    console.log("sendData ", sendData);
+    setLoading(false);
+  }, [sendData]);
 
   return (
     <form
@@ -22,7 +72,7 @@ const SearchBar = () => {
         type="search"
         onChange={handleChange}
         placeholder="Search here"
-        className="bg-gray-50 focus:outline-none focus-within:border-blue-700"
+        className="bg-gray-50 focus:outline-none focus:border-blue-700"
       />
       <button
         type="submit"
