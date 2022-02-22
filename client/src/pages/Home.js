ca44a796977ae83de645ac8025fcc07f3af45595
Izzy A <IzzyAkram@outlook.com>
2022-02-22 18:13:19 -0500

    Landing page touch ups

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,10 +18,9 @@ export default function Home() {
 
                     <div className="">
                         <div className={`py-8 px-4 shadow sm:rounded-lg sm:px-10 w-full`}>
-                            <p><h1 className=" text-3xl">Good health starts with what you eat.</h1> <br></br>
-                            Want to eat more mindfully? <br></br>
-                            Track meals, learn about your habits, and reach your goals with Nutrimental. <br></br>
-                            Sign up today!</p>
+                            <p><h1 className=" text-3xl">We can help you in achieving your goals.</h1> <br></br>
+                            Nutrimental is here to guide every step. <br></br>
+                            Track meals, observe your habits, and achieve your goals!</p>
                         </div>
                     </div>
                 </div>
@@ -30,8 +29,9 @@ export default function Home() {
                 <div className="flex justify-center py-8 sm:px-6 lg:px-8 w-auto sm:mx-24 md:mx-44 lg:mx-44">
                 <div className="">
                         <div className={`py-8 px-4 shadow sm:rounded-lg sm:px-10 w-full`}>
-                            <p><h1 className=" text-3xl">Search from over 14 million foods.</h1> <br></br>
-                            See a breakdown of calories and nutrients, compare serving sizes, and discover how the food you eat supports your goals. <br></br>
+                            <p><h1 className=" text-3xl">Log food items into our tracker.</h1> <br></br>
+                            Search food items, see a breakdown of calories and nutrients, and compare portion sizes<br></br>
+                            Then taylor dietary habits according to our feedback.<br></br>
                             </p>
                         </div>
                     </div>
