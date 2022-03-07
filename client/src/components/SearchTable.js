6834ca4f6832f32f22cbb235c1a267789c6e5316
Justin <justinparkcs@gmail.com>
2022-03-07 01:37:09 -0500

    feat: sortable table details in searched page

--- client/src/components/SearchTable.js
+++ client/src/components/SearchTable.js
@@ -1,98 +1,48 @@
 import React from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
+import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
-  { id: "nutrientName", label: "Vitamin Name", minWidth: 170 },
   {
-    id: "value",
-    label: "Vitamin Amount",
+    field: "nutrientName",
+    property: "Vitamin Name",
     minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
+    // align: "center",
   },
   {
-    id: "unitName",
-    label: "Vitamin Amount Unit",
+    field: "value",
+    property: "Vitamin Amount",
     minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
+    // align: "center",
+    // editable: true,
+  },
+  {
+    field: "unitName",
+    property: "Vitamin Amount Unit",
+    minWidth: 170,
+    // align: "center",
   },
 ];
 
 const SearchTable = ({ data }) => {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const newData = data.filter((item) => item.value > 0);
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+  newData.forEach((item, i) => {
+    item.id = i + 1;
+  });
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {newData
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, i) => {
-                return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={`${i} ${row.nutrientId}`}
-                  >
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 15, 20]}
-        component="div"
-        count={newData.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+    <div
+      className="relative flex h-96 w-7/12 mx-auto overflow-hidden"
+      // style={{ height: 400, width: "100%" }}
+    >
+      <DataGrid
+        rows={newData}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        disableSelectionOnClick
+        className="absolute pl-4 "
       />
-    </Paper>
+    </div>
   );
 };
 
