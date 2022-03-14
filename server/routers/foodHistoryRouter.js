1a9d3dfa986e03bf78f491f6711a5348b8281e18
tanzil7 <tbaraskar191999@gmail.com>
2022-03-14 18:16:32 -0400

    feat: put router & table implemented

--- server/routers/foodHistoryRouter.js
+++ server/routers/foodHistoryRouter.js
@@ -59,14 +59,43 @@ foodHistoryRouter.post(
 foodHistoryRouter.delete(
   "/delete/",
   expressAsyncHandler(async (req, res, err) => {
-    foodHistoryModel.update(
-      { user_email: req.body.user_email },
-      { $pull: { history: { _id: req.body["_id"] } } },
+    foodHistoryModel.updateOne(
+      { user_email: req.query.user_email },
+      { $pull: { history: { _id: req.query._id } } },
       function (err, result) {
-        if (err) res.send(400);
-        else return res.send(200);
+        if (err) {
+          res.send(400);
+        } else {
+          return res.send(200);
+        }
       }
     );
+  })
+);
+foodHistoryRouter.put(
+  "/put/",
+  expressAsyncHandler(async (req, res, err) => {
+    try {
+      await foodHistoryModel.findOne(
+        { user_email: req.body.user_email },
+        function (err, result) {
+          if (err) res.send(400);
+          else {
+            // update subdocument in mongoose
+            var update = result.history.filter((obj) => {
+              return obj._id == req.body._id;
+            });
+            update[0].food_name = req.body.food_name;
+            update[0].calories = req.body.calories;
+            update[0].amount = req.body.amount;
+            result.save();
+            return res.send(200);
+          }
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
   })
 );
 
