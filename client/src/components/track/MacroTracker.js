cb13d9a2432216153f6077ed3fa941aacfb3b4f8
Justin <justinparkcs@gmail.com>
2022-03-13 21:46:44 -0400

    feat: full data output. (bug: data in table turning zero)

--- client/src/components/track/MacroTracker.js
+++ client/src/components/track/MacroTracker.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import Table from "react-bootstrap/Table";
+import { DataGrid } from "@mui/x-data-grid";
 // import ProgressBar from "react-bootstrap/ProgressBar";
 
 const capitalizeFirstLetter = (string) => {
@@ -19,46 +19,85 @@ const getAge = (dateString) => {
   return age;
 };
 
+const columns = [
+  {
+    field: "vitamin_name",
+    property: "Macro Name",
+    minWidth: 170,
+    // align: "center",
+  },
+  {
+    field: "nutrient_quantity",
+    property: "Amount Consumed",
+    minWidth: 170,
+    // align: "center",
+  },
+  {
+    field: "recommended_amount",
+    property: "Recommended Amount",
+    minWidth: 170,
+    // align: "center",
+    // editable: true,
+  },
+  {
+    field: "amount_remaining",
+    property: "Amount Remaining",
+    minWidth: 170,
+    // align: "center",
+  },
+  {
+    field: "upper_tolerable_limit",
+    property: "Upper Tolerable Limit",
+    minWidth: 170,
+    // align: "center",
+  },
+];
+
 export default function MacroTracker({ authProps, date }) {
   const [intakeHistory, setIntakeHistory] = useState([]);
   const [dailyIntake, setDailyIntake] = useState([]);
-  // const []
-  // const [calories, setCalories] = useState(0);
-  // const [nutrientIntake, setNutrientIntake] = useState([]);
+  const [finalData, setFinalData] = useState([]);
 
   const getIntakeHistory = (resData) => {
     let intaken_list = [];
     // for each food in the day's food history
     resData.forEach((dataObjs) => {
-      let single_food = []
+      let single_food = [];
       // Getting calorie counts
       let intaken_item = {
         vitamin_name: "calories",
-        nutrient_quantity: Number((parseFloat(dataObjs.calories)).toFixed(1))
+        nutrient_quantity: Number(parseFloat(dataObjs.calories).toFixed(1)),
       };
-      single_food.push(intaken_item)
+      single_food.push(intaken_item);
       // Getting each nutrient amount
       dataObjs.nutrients.forEach((nut_item) => {
         intaken_item = {
-          vitamin_name: nut_item.nutrient_name.split(',')[0].replaceAll('-', ''),
-          nutrient_quantity: parseFloat(nut_item.nutrient_quantity)
+          vitamin_name: nut_item.nutrient_name
+            .split(",")[0]
+            .replaceAll("-", ""),
+          nutrient_quantity: parseFloat(nut_item.nutrient_quantity),
         };
-        single_food.push(intaken_item)
+        single_food.push(intaken_item);
       });
 
-      // accumulating foods 
+      // accumulating foods
       if (intaken_list.length > 0) {
         intaken_list = Object.values(
-          intaken_list.concat(single_food).reduce((acc, {nutrient_quantity, vitamin_name})=>{
-            (acc[vitamin_name] ??= {vitamin_name, nutrient_quantity: 0}).nutrient_quantity += parseFloat(nutrient_quantity);
-            return acc;
-          }, {})
+          intaken_list
+            .concat(single_food)
+            .reduce((acc, { nutrient_quantity, vitamin_name }) => {
+              (acc[vitamin_name] ??= {
+                vitamin_name,
+                nutrient_quantity: 0,
+              }).nutrient_quantity += parseFloat(nutrient_quantity);
+              return acc;
+            }, {})
         );
       } else {
-        intaken_list = single_food
+        intaken_list = single_food;
       }
     });
-    return intaken_list
+    return intaken_list;
   };
 
   const getIntakeHistoryData = async () => {
@@ -69,8 +108,7 @@ export default function MacroTracker({ authProps, date }) {
         }&date=${date.toLocaleDateString().replaceAll("/", "-")}`
       )
       .then((res) => {
-        setIntakeHistory(
-          getIntakeHistory(res.data));
+        setIntakeHistory(getIntakeHistory(res.data));
       })
       .catch((err) => {
         toast.error(err.message);
@@ -146,44 +184,99 @@ export default function MacroTracker({ authProps, date }) {
       });
   };
 
+  const getFinalData = (arr1, arr2) => {
+    let id = 0;
+    let merged = [];
+    for (let i = 0; i < arr1.length; i++) {
+      merged.push({
+        ...arr1[i],
+        ...arr2.find(
+          (obj_item) => obj_item.vitamin_name === arr1[i].vitamin_name
+        ),
+      });
+    }
+    if (merged.length > 0) {
+      merged.forEach((obj) => {
+        id++;
+        obj["id"] = id;
+        if (!obj.hasOwnProperty("nutrient_quantity")) {
+          obj["nutrient_quantity"] = 0;
+        }
+        let ra = obj.recommended_amount;
+        let nq = obj.nutrient_quantity;
+        obj["amount_remaining"] = ra - nq;
+      });
+      setFinalData(merged);
+    }
+  };
+
   useEffect(() => {
     getIntakeHistoryData();
     getDailyIntakeData();
   }, [date]);
 
-  return (
-    <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-      <Table className="min-w-full divide-y divide-gray-300">
-        <thead>
-          <tr>
-            <th>Macro</th>
-            <th>Total</th>
-            <th>Goal</th>
-            <th>Left</th>
-          </tr>
-        </thead>
+  useEffect(() => {
+    getFinalData(dailyIntake, intakeHistory);
+  }, [dailyIntake]);
 
-        <tbody className="divide-y divide-gray-200 bg-white">
-          {/* {console.log("dailyIntake: ", dailyIntake)} */}
-          {dailyIntake.length >= 35 && (
-            <>
-              <tr className="h-16 m-auto" >
-                <td>
-                  <div>
-                    {console.log('dailyIntake', dailyIntake)}
-                    {console.log('intakeHistory', intakeHistory)}
-                    <p>Data 1</p>
-                    <p className="absolute">@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@</p>
-                  </div>
-                </td>
-                <td>Data 2</td>
-                <td>Data 3</td>
-                <td>Data 4</td>
-              </tr>
-            </>
-          )}
-        </tbody>
-      </Table>
+  return (
+    <div
+      className="relative flex h-96 w-full mx-auto"
+      // style={{ height: 400, width: "100%" }}
+    >
+      {dailyIntake.length < 35 ? (
+        <>Loading</>
+      ) : (
+        <DataGrid
+          rows={finalData}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          disableSelectionOnClick
+          // className="absolute pl-4 "
+        />
+      )}
     </div>
+
+    // <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+    //   <Table className="min-w-full divide-y divide-gray-300">
+    //     <thead>
+    //       <tr>
+    //         <th>Macro Name</th>
+    //         <th>Total (mg)</th>
+    //         <th>Goal (mg)</th>
+    //         <th>Left (mg)</th>
+    //         <th>Max (mg)</th>
+    //       </tr>
+    //     </thead>
+
+    //     <tbody className="divide-y divide-gray-200 bg-white">
+    //       {/* {console.log("dailyIntake: ", dailyIntake)} */}
+    //       {dailyIntake.length >= 35 &&
+    //         dailyIntake.forEach((obj) => {
+    //           <>
+    //             <tr className="h-16 m-auto" key={obj.vitamin_name}>
+    //               <td>
+    //                 <div>
+    //                   {console.log(obj)}
+    //                   {/* {console.log("dailyIntake", dailyIntake)}
+    //                     {console.log("intakeHistory", intakeHistory)} */}
+    //                   <p>{obj.vitamin_name}</p>
+    //                   <p className="absolute">
+    //                     PROGRESS_BAR PROGRESS_BAR PROGRESS_BAR PROGRESS_BAR
+    //                     PROGRESS_BAR PROGRESS_BAR{" "}
+    //                   </p>
+    //                 </div>
+    //               </td>
+    //               <td>Total Data</td>
+    //               <td>{obj.recommended_amount}</td>
+    //               <td>Remaining Data</td>
+    //               <td>{obj.upper_tolerable_limit}</td>
+    //             </tr>
+    //           </>;
+    //         })}
+    //     </tbody>
+    //   </Table>
+    // </div>
   );
 }
