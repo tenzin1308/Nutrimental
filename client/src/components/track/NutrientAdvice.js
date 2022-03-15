9a10c58297976778d5a7bdaafb45bccd8f88f6b3
Justin <justinparkcs@gmail.com>
2022-03-14 22:39:26 -0400

    checkpoint

--- client/src/components/track/NutrientAdvice.js
+++ client/src/components/track/NutrientAdvice.js
@@ -1,149 +1,163 @@
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import SearchIcon from '@mui/icons-material/Search';
-import { InputBase, Paper } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import toast from 'react-hot-toast';
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import SearchIcon from "@mui/icons-material/Search";
+import { InputBase, Paper } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import toast from "react-hot-toast";
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default function NutrientAdvice() {
+  const [searchText, setSearchText] = useState("");
 
-    const [searchText, setSearchText] = useState('');
+  const [keywords, setKeywords] = useState([]);
 
-    const [keywords, setKeywords] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [vitaminList, setVitaminList] = useState([]);
 
-    const [foods, setFoods] = useState([]);
-    const [vitaminList, setVitaminList] = useState([]);
+  const getAllVitamins = async () => {
+    await axios
+      .get("/api/vitamin/get-all/")
+      .then((res) => {
+        setVitaminList(res.data);
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  };
+  useEffect(() => {
+    getAllVitamins();
+  }, [keywords]);
 
-    
-    const getAllVitamins = async()=> {
-        await axios.get("/api/vitamin/get-all/")
-        .then(res => {
-            setVitaminList(res.data)
-        })
-        .catch(err => {
-            toast.error(err.message)
-        })
-    };
-    useEffect(() => {
-        getAllVitamins()
-    }, [keywords])
-    
-    // useEffect to run on hook change (nutrients)          ORIGINAL
-    useEffect( () => {
-        keywords.forEach(async(keyword) => {
-            await axios.get(`/api/advice/get?search=${keyword}`)
-            .then(res => {
-                let aux_obj = {
-                    keyword: keyword, 
-                    vitamins: [] // this will be nested vitamins: [ { vitamin_name: vitamin_name, foods: res.data[0].foods } ]
-                }
-                
-                // let aux_aux2 = {
-                //     vitamin_name: vitamin,
-                //     foods: []
-                // }
-                res.data[0].vitamins.forEach(item => {
-                    aux_obj.vitamins.push(capitalizeFirstLetter(item))
-                    
-                    
-                    //aux_obj.vitamins[item] = [foods]
-                    
-                })
-                console.log("vitamin_list",vitaminList)
-                console.log('aux_obj',aux_obj)
-                console.log('res data', res.data[0])
-                // aux_obj.vitamins.forEach(vitamin => {
-                //     let aux_vitamin_object = {
-                //         vitamin_name: vitamin,
-                //         function: "",
-                //         foods: []
-                //     }
-                //     let merged = 
-                //     // if(vitaminList.some(i => i.vitamin_name.includes(vitamin))){
-                //     //     console.log("Theres a matched on vitaminList with me! :", vitamin)
-                //     //     //console.log(vitaminList.vitamin_name.indexOf(vitamin))
-                        
-                //     // } else {
-                //     //     console.log("There is NO MATCH for me:", vitamin)
-                //     // }
-                // })
-                
-                
-            })
-            .catch(err => {
-                //console.log('err',err)
-                toast.error(err.message);
-            })
-            // aux_list.push(aux_data)
-            
-        })
-    }, [vitaminList])
+  // useEffect to run on hook change (nutrients)          ORIGINAL
+  useEffect(() => {
+    keywords.forEach(async (keyword) => {
+      await axios
+        .get(`/api/advice/get?search=${keyword}`)
+        .then((res) => {
+          let aux_obj = {
+            keyword: keyword,
+            vitamins: [], // this will be nested vitamins: [ { vitamin_name: vitamin_name, foods: res.data[0].foods } ]
+          };
 
-        
-        
+          // let aux_aux2 = {
+          //     vitamin_name: vitamin,
+          //     foods: []
+          // }
+          res.data[0].vitamins.forEach((item) => {
+            aux_obj.vitamins.push(capitalizeFirstLetter(item));
 
-    // useEffect to run on hook change (food name)                    Perhaps can change this useEffect to have dependency be on Foods
-    useEffect(() => {
-        console.log(foods)
-        
-        
-    },[foods])
-
-
-    const searchHandler = () => {
-        const keyWordsList = [
-            'brain','eyes','skin','teeth','bone','muscles',
-            'lung','liver','heart','digestive system','immune system','kidneys',
-            'reproductive system','ears','hair','blood','breasts','nervous system']
-
-        let userInputSplit = searchText.split(" ")
-
-        // Filters out keyword(s) from searchText and place into array to be used 
-        let matchedWords = []
-        userInputSplit.map((word) => {
-            if(keyWordsList.includes(word)){
-                matchedWords.push(word)
+            //aux_obj.vitamins[item] = [foods]
+          });
+          console.log("vitamin_list", vitaminList);
+          console.log("aux_obj", aux_obj);
+          console.log("res data", res.data[0]);
+          // aux_obj.vitamins.forEach(vitamin => {
+          //     let aux_vitamin_object = {
+          //         vitamin_name: vitamin,
+          //         function: "",
+          //         foods: []
+          //     }
+          //     let merged =
+          let aux_vitamins = [];
+          aux_obj.vitamins.forEach((vitamin) => {
+            let finds = vitaminList.find((element) => {
+              if (element.vitamin_name.includes(vitamin)) {
+                return true;
+              }
+            });
+            if (finds) {
+              aux_vitamins.push(finds);
             }
-        })
-        
-        setKeywords(matchedWords)
+            // console.log("finds@@@@", finds);
+          });
+          aux_obj["vitamins_info_list"] = aux_vitamins;
+          console.log("aux_obj", aux_obj);
+          //     // if(vitaminList.some(i => i.vitamin_name.includes(vitamin))){
+          //     //     console.log("Theres a matched on vitaminList with me! :", vitamin)
+          //     //     //console.log(vitaminList.vitamin_name.indexOf(vitamin))
 
-    }
-    const clearTextHandler = () => {
-        setSearchText('');
-    }
-    
-    
-    
-    return (
-        <Paper
-            // component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
-            // onSubmit={searchHandler}
-        >
-            
-            <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                id="how-can-i-help-you"
-                placeholder='How Can I Help You?'
-                inputProps={{ 'aria-label': 'How Can I Help You?' }}
-                onChange={(e) => setSearchText(e.target.value)}
-                value={searchText ? searchText : ''}
-            />
-            {searchText !== '' &&
-                <IconButton aria-label='delete' onClick={clearTextHandler}>
-                    <HighlightOffIcon />
-                </IconButton>
-            }
-            <IconButton aria-label="search" onClick={searchHandler}>
-                <SearchIcon />
-            </IconButton>
-            
-        </Paper>
-  )
+          //     // } else {
+          //     //     console.log("There is NO MATCH for me:", vitamin)
+          //     // }
+          // })
+        })
+        .catch((err) => {
+          //console.log('err',err)
+          toast.error(err.message);
+        });
+      // aux_list.push(aux_data)
+    });
+  }, [vitaminList]);
+
+  // useEffect to run on hook change (food name)                    Perhaps can change this useEffect to have dependency be on Foods
+  useEffect(() => {
+    console.log(foods);
+  }, [foods]);
+
+  const searchHandler = () => {
+    const keyWordsList = [
+      "brain",
+      "eyes",
+      "skin",
+      "teeth",
+      "bone",
+      "muscles",
+      "lung",
+      "liver",
+      "heart",
+      "digestive system",
+      "immune system",
+      "kidneys",
+      "reproductive system",
+      "ears",
+      "hair",
+      "blood",
+      "breasts",
+      "nervous system",
+    ];
+
+    let userInputSplit = searchText.split(" ");
+
+    // Filters out keyword(s) from searchText and place into array to be used
+    let matchedWords = [];
+    userInputSplit.map((word) => {
+      if (keyWordsList.includes(word)) {
+        matchedWords.push(word);
+      }
+    });
+
+    setKeywords(matchedWords);
+  };
+  const clearTextHandler = () => {
+    setSearchText("");
+  };
+
+  return (
+    <Paper
+      // component="form"
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}
+      // onSubmit={searchHandler}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        id="how-can-i-help-you"
+        placeholder="How Can I Help You?"
+        inputProps={{ "aria-label": "How Can I Help You?" }}
+        onChange={(e) => setSearchText(e.target.value)}
+        value={searchText ? searchText : ""}
+      />
+      {searchText !== "" && (
+        <IconButton aria-label="delete" onClick={clearTextHandler}>
+          <HighlightOffIcon />
+        </IconButton>
+      )}
+      <IconButton aria-label="search" onClick={searchHandler}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 }
