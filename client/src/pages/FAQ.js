eddf380d6cbe5d8c9f014e96a400ec119d328117
tanzil7 <tbaraskar191999@gmail.com>
2022-05-09 20:15:32 -0400

    fix FAQ page

--- client/src/pages/FAQ.js
+++ client/src/pages/FAQ.js
@@ -1,11 +1,11 @@
-import * as React from "react";
+import React from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-export default function ControlledAccordions() {
+export default function FAQ() {
   const [expanded, setExpanded] = React.useState(false);
   const [panels, setPanels] = React.useState(
     "ans1",
